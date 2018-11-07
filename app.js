3f9d12b6626b388eadd40cdfbbc9bfe6b7385951
Matthew <morch028@uottawa.ca>
2018-11-07 18:20:42 -0500

    Add basic user creation and login functionality

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+
+mongoose.connect(process.env.MONGO_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () =>
+  console.log("Connected to mongo")
+);
+
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../common/models/User');
+const router = express.Router();
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.json([{
     id: 1,
     username: "Mert"
   }]);
+});
+
+router.post('/', async function(req, res) {
+  const {email, username, password} = req.body;
+  if (!email || !username || !password) {
+    return res.status(412).send("Account must have email, username, and password");
+  }
+  console.log(`Creating account: ${email}, ${username}`);
+  try {
+    const createdUser = await User.create({email, username, password});
+    console.log("Account created!");
+    res.json({id: createdUser._id, username, email});
+  } catch (error) {
+    console.error("Account not created", error);
+  }
+});
+
+router.post('/login', async function(req, res) {
+  const {username, password} = req.body;
+  try {
+    const user = await User.authenticate(username, password);
+    res.json(user);
+  } catch(error) {
+    console.error(error);
+    res.status(401).send("Incorrect username / password")
+  }
 });
 
 module.exports = router;
