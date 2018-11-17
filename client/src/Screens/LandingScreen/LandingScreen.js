fcd3a6e2e98297c4640d965df630208f213ca19f
morch028 <morch028@uottawa.ca>
2018-11-17 18:51:05 -0500

    Add landing screen and basic routes

--- client/src/Screens/LandingScreen/LandingScreen.js
+++ client/src/Screens/LandingScreen/LandingScreen.js
@@ -1,7 +1,25 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
 
 export default class LandingScreen extends Component {
   render() {
-    return <div/>;
+    return <main>
+      Welcome to Houses of Dreams Online Property Rental System
+      <ul>
+        <li>
+          <Link to="/my-properties">My Properties</Link>
+        </li>
+        <li>
+          <Link to="/my-account">My Account</Link>
+        </li>
+        <li>
+          <Link to="/browse">Browse Properties</Link>
+        </li>
+        <li>
+          <Link to="/visiting-list">Visiting List</Link>
+        </li>
+      </ul>
+    </main>;
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,22 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import LoginScreen from "./Screens/LoginScreen/LoginScreen";
 import LandingScreen from "./Screens/LandingScreen/LandingScreen";
+import MyPropertiesScreen from "./Screens/MyPropertiesScreen/MyPropertiesScreen";
+import UserScreen from "./Screens/UserScreen/UserScreen";
+import CreateUserScreen from "./Screens/CreateUserScreen/CreateUserScreen";
+import BrowsePropertiesScreen from "./Screens/BrowsePropertiesScreen/BrowsePropertiesScreen";
+import VisitingListScreen from "./Screens/VisitingListScreen/VisitingListScreen";
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
       <Route path="/" component={LandingScreen} exact={true}/>
       <Route path="/login" component={LoginScreen}/>
+      <Route path="/my-properties" component={MyPropertiesScreen}/>
+      <Route path="/my-account" component={UserScreen}/>
+      <Route path="/create-account" component={CreateUserScreen}/>
+      <Route path="/browse" component={BrowsePropertiesScreen}/>
+      <Route path="/visiting-list" component={VisitingListScreen}/>
     </div>
   </BrowserRouter>,
   document.getElementById('root'));
