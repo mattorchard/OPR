a169406602a5402a4566d92f44bbb9b4bb6d1dd1
Matthew <morch028@uottawa.ca>
2018-11-19 16:14:57 -0500

    Move router from index to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import LoginScreen from './Screens/LoginScreen/LoginScreen';
-// import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+import {BrowserRouter, Route} from "react-router-dom";
+import LandingScreen from "./Screens/LandingScreen/LandingScreen";
+import MyPropertiesScreen from "./Screens/MyPropertiesScreen/MyPropertiesScreen";
+import UserScreen from "./Screens/UserScreen/UserScreen";
+import CreateUserScreen from "./Screens/CreateUserScreen/CreateUserScreen";
+import BrowsePropertiesScreen from "./Screens/BrowsePropertiesScreen/BrowsePropertiesScreen";
+import VisitingListScreen from "./Screens/VisitingListScreen/VisitingListScreen";
 
 
-class App extends Component {
-  state = { users: []};
 
-  async componentDidMount() {
-    const response = await fetch("/users");
-    this.setState({users: await response.json()});
-  }
+export default class App extends Component {
 
   render() {
-    return (
-      <LoginScreen/>
-    );
+    return <BrowserRouter>
+      <div>
+        <Route path="/" component={LandingScreen} exact={true}/>
+        <Route path="/login" component={LoginScreen}/>
+        <Route path="/my-properties" component={MyPropertiesScreen}/>
+        <Route path="/my-account" component={UserScreen}/>
+        <Route path="/create-account" component={CreateUserScreen}/>
+        <Route path="/browse" component={BrowsePropertiesScreen}/>
+        <Route path="/visiting-list" component={VisitingListScreen}/>
+      </div>
+    </BrowserRouter>
   }
 }
-
-export default App;
