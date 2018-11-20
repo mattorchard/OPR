c862d71330718626336debb10c13e7aef22f4bae
morch028 <morch028@uottawa.ca>
2018-11-20 13:54:26 -0500

    Add initial account badge

---
+++ client/src/NavBar/MyAccountBadge.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+import LogoutButton from "../Shared/LogoutButton";
+
+
+export default class MyAccountBadge extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.letter = this.props.role.charAt(0).toUpperCase();
+  }
+
+  render() {
+    return <div className="my-account-badge">
+      <button
+        type="button"
+        onClick={() => this.setState(oldState => ({open: !oldState.open}))}
+        className={`my-account-badge__toggle avatar--${this.props.role}`}>
+        {this.letter}
+      </button>
+      <div className={`my-account-badge__popover my-account-badge__popover--${this.state.open ? "open" : "closed"}`}>
+        <ul>
+          <li>Signed in as: <strong>{this.props.username}</strong></li>
+          <li>{this.props.givenName}</li>
+          <li>{this.props.lastName}</li>
+          <li>{this.props.role}</li>
+          <li><LogoutButton/></li>
+        </ul>
+      </div>
+    </div>
+  }
+}
--- client/src/NavBar/NavBar.js
+++ client/src/NavBar/NavBar.js
@@ -1,14 +1,14 @@
 import React, {Component} from "react";
-import LogoutButton from "./LogoutButton";
 import Link from "react-router-dom/es/Link";
 import {UserConsumer} from "../Contexts/UserContext";
-
+import MyAccountBadge from "./MyAccountBadge";
+import "./NavBar.css";
 
 export default class NavBar extends Component {
   render() {
     return <UserConsumer>
-      {({authenticated})=>(
-        authenticated ? <LogoutButton/> : <Link to="/login">Login</Link>
+      {({authenticated, user})=>(
+        authenticated ? <MyAccountBadge {...user}/> : <Link to="/login">Login</Link>
       )}
     </UserConsumer>
   }
