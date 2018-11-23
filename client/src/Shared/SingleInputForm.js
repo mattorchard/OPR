294472f3e52cc721d05e2ec90703c911eeccf255
Matthew <morch028@uottawa.ca>
2018-11-22 19:55:31 -0500

    Adds single input form

---
+++ client/src/Shared/SingleInputForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactForm from "./ReactForm";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+export default class SingleInputForm extends ReactForm {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      value: props.value || ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderInput = this.renderInput.bind(this);
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    if (this.props.onSubmit) {
+      try {
+        await this.props.onSubmit(this.state.value);
+        this.setState({open: false});
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+  }
+
+  renderInput() {
+    return React.cloneElement(
+      React.Children.only(this.props.children), {
+        required: true,
+        name: "value",
+        value: this.state.value,
+        onChange: this.handleInputChange
+      });
+  }
+
+  render() {
+    if (this.state.open) {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput()}
+          <button
+            type="submit"
+            aria-label="Submit">
+            <FontAwesomeIcon icon="check" />
+          </button>
+          <button
+            type="reset"
+            aria-label="Cancel"
+            onClick={() => this.setState({open: false, value: ""})}>
+            <FontAwesomeIcon icon="times" />
+          </button>
+        </form>
+      );
+    }
+    else {
+      return (
+        <button onClick={() => this.setState({open: true})}>
+          {this.props.label}
+        </button>
+      );
+    }
+  }
+}
