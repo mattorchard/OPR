5596b546003e0f8428c014952792c24fb442c107
morch028 <morch028@uottawa.ca>
2018-11-25 17:56:21 -0500

    Fix property formatting and add apartment and unit number constraint

--- routes/properties.js
+++ routes/properties.js
@@ -3,19 +3,25 @@ const {Property} = require('../common/models/Property');
 const router = express.Router();
 const {hasRole} = require("../custom_middleware/authorization");
 
+
 router.post('/', hasRole("owner"), async function(req, res) {
   try {
+    const {type, address} = req.body;
+    if (type === "apartment" && !address.unitNumber) {
+      return res.status(412).send("Apartments must include a unit number");
+    }
     const property = await Property.create({
       ...req.body,
       createdOn: Date.now(),
       ownerId: req.user._id
     });
     console.log(`Created address for owner ${req.user.username}`);
-    return res.json(property);
+    return res.json(property.toObject());
   } catch (error) {
     if (error.name === "ValidationError") {
       return res.status(412).send(error.message);
     } else {
+      console.error("Unable to create property", error);
       return res.status(500).send("Unable to create property");
     }
   }
