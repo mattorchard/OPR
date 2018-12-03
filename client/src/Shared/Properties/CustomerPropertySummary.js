cd3bb3b77c3f9b1da7ac72760e97ea80dd5cf220
morch028 <morch028@uottawa.ca>
2018-12-02 22:54:12 -0500

    Fix class definitions

--- client/src/Shared/Properties/CustomerPropertySummary.js
+++ client/src/Shared/Properties/CustomerPropertySummary.js
@@ -3,7 +3,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import PropertySummary from "./PropertySummary";
 
 
-export default class OwnerPropertySummary extends PropertySummary {
+export default class CustomerPropertySummary extends PropertySummary {
   constructor(props) {
     super(props);
     this.actionRow = <div className="pull-right">
