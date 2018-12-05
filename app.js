3fc78b4845e318d0a1b5a3c3f7b850632b86f268
Matthew <morch028@uottawa.ca>
2018-12-05 11:01:27 -0500

    Serve only index route to allow react-router to determine sub pages

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const propertiesRouter = require('./routes/properties');
 const visitingListRouter = require('./routes/visiting-list');
@@ -27,13 +26,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', indexRouter);
+app.use('/static', express.static(path.join(__dirname, 'client/build/static')));
+
+
 app.use('/users', usersRouter);
 app.use('/properties', propertiesRouter);
 app.use('/visitingList', visitingListRouter);
 app.use('/photos', photoRouter);
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'client/build/')});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
